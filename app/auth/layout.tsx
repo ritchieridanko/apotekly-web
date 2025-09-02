3b2a26f0df0f59ff9bd1da719d89276532ecb815
Ritchie Ridanko <ritchieridanko@gmail.com>
2025-09-02 13:59:37 +0700

    fix: readjust /auth layout

--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -2,7 +2,7 @@ import { FC, ReactNode } from "react";
 
 const AuthLayout: FC<{ children: ReactNode }> = ({ children }) => {
   return (
-    <div className="h-screen w-full flex justify-center items-center scrolling-auth-bg overflow-y-auto">
+    <div className="py-[30px] min-h-screen w-full flex justify-center items-center scrolling-auth-bg overflow-y-auto">
       <div className="p-[30px] w-[450px] bg-white rounded-sm ring-8 ring-black/30">
         {children}
       </div>
